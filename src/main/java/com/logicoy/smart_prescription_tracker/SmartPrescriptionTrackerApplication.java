a354ee67f7ed203110d8df3adb6351f435c7e5a5
Nayeem Khan <nayeemkhanpathan9@gmail.com>
2026-02-02 00:05:18 +0530

    chore: bootstrap Spring Boot project with base dependencies

---
+++ src/main/java/com/logicoy/smart_prescription_tracker/SmartPrescriptionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.logicoy.smart_prescription_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPrescriptionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPrescriptionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logicoy/smart_prescription_tracker/SmartPrescriptionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicoy.smart_prescription_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPrescriptionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
